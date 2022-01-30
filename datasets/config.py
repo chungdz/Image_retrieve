5f091708db129c70cd149f96efe31f65ac22dbd0
xiaoxin Hu <1033718596@qq.com>
2022-01-30 12:49:29 -0800

    change names

---
+++ datasets/config.py
@@ -0,0 +1,13 @@
+import json
+import pickle
+import numpy as np
+
+class ModelConfig():
+    def __init__(self):
+
+        self.h = 224
+        self.w = 224
+        self.neg_count = 4
+        self.hidden_size = 100
+        self.dropout = 0.5
+
