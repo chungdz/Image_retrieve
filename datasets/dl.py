88cf1379a23184434305dbd44a315db9677522f6
xiaoxin Hu <1033718596@qq.com>
2022-02-09 15:46:29 -0800

    dl made up

--- datasets/dl.py
+++ datasets/dl.py
@@ -2,28 +2,17 @@ from torch.utils.data.dataset import Dataset
 from tqdm import tqdm
 import torch
 import pandas as pd
+import numpy as np
  
 class FNNData(Dataset):
-    def __init__(self, data_path, pic_path, test_mode=False):
-        self.tm = test_mode
-        self.to_drop_list = ['shipment_method_id', 'category_id', 'package_size', 'sender_state', 'receive_state']
-        df = pd.read_csv(data_path, sep='\t')
-
-        if test_mode:
-            self.x = torch.FloatTensor(df.drop(['record_number'] + self.to_drop_list, axis=1).values)
-        else:
-            self.x = torch.FloatTensor(df.drop(['target', 'record_number'] + self.to_drop_list, axis=1).values)
-            self.y = torch.LongTensor(df.target.values).unsqueeze(-1)
-        
-        self.record_number = torch.FloatTensor(df.record_number.values).unsqueeze(-1)
-        self.x2 = torch.LongTensor(df[self.to_drop_list].values)
+    def __init__(self, cfg, dataset):
+        self.cfg = cfg
+        self.pic_matrix = cfg.pic_matrix
+        self.dataset = np.load(dataset)
 
     def __getitem__(self, index):
-        if self.tm:
-            return torch.cat([self.x[index], self.x2[index], self.record_number[index]])
-        else:
-            return torch.cat([self.x[index], self.x2[index], self.y[index], self.record_number[index]])
+        return torch.FloatTensor(self.pic_matrix[self.dataset[index]])
  
     def __len__(self):
-        return self.x.size(0)
+        return self.dataset.shape[0]
 
