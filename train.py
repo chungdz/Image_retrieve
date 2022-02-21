5372b8ce3075ebf7d3e79f609157dbc05287edca
xiaoxin Hu <1033718596@qq.com>
2022-02-21 10:32:01 -0800

    first version of train

--- train.py
+++ train.py
@@ -3,17 +3,19 @@ import json
 import pandas as pd
 from tqdm import trange
 import argparse
-from modules.classical_multi import MultiResNet
+from datasets.config import GeMConfig
+from modules.gem import GeM
 from utils.train_util import set_seed
 from torch.utils.data import DataLoader
-from datasets.dl import FNNData
+from datasets.dl import GeMData
 import torch
 import os
 from tqdm import tqdm
 import gc
 import torch.nn.functional as F
+from sklearn.metrics import roc_auc_score
 
-def run(cfg, train_dataset, valid_dataset, fp):
+def run(cfg, train_dataset, valid_dataset):
     """
     train and evaluate
     :param args: config
@@ -29,29 +31,19 @@ def run(cfg, train_dataset, valid_dataset, fp):
     valid_data_loader = DataLoader(valid_dataset, batch_size=cfg.batch_size, shuffle=False)
 
     # Build model.
-    model = FNNData(cfg)
+    model = GeMData(cfg.model_info)
+    model.to(0)
     # Build optimizer.
     steps_one_epoch = len(train_data_loader)
     train_steps = cfg.epoch * steps_one_epoch
     print("Total train steps: ", train_steps)
     optimizer = torch.optim.Adam(params=model.parameters(), lr=cfg.lr)
-    min_ebay = -1
     # Training and validation
     
     for epoch in range(cfg.epoch):
         # print(model.match_prediction_layer.state_dict()['2.bias'])
         train(cfg, epoch, model, train_data_loader, optimizer, steps_one_epoch)
-        eval_return, rc, preds = validate(cfg, model, valid_data_loader)
-        print(epoch, eval_return)
-        
-        if min_ebay == -1 or eval_return < min_ebay:
-            min_ebay = eval_return
-            min_rc = rc
-            min_pred = preds
-            torch.save(model.state_dict(), fp)
-        
-    return min_ebay, min_rc, min_pred
-
+        validate(cfg, model, valid_data_loader)
 
 def train(cfg, epoch, model, loader, optimizer, steps_one_epoch):
     model.train()
@@ -59,21 +51,18 @@ def train(cfg, epoch, model, loader, optimizer, steps_one_epoch):
     enum_dataloader = tqdm(loader, total=len(loader), desc="EP-{} train".format(epoch))
     index = 0
     mean_loss = 0
+    input_label = torch.zeros((cfg.batch_size), dtype=torch.long).to(0)
+
     for data in enum_dataloader:
-        if index >= steps_one_epoch:
-            break
-        
         # 1. Forward
-        pred = model(data[:, :-2]).squeeze()
-        loss = F.cross_entropy(pred, data[:, -2].long())
+        data = data / 256.0
+        data = data.to(0)
+        pred = model(data, 224)
+        loss = F.cross_entropy(pred, input_label)
 
         # 3.Backward.
         loss.backward()
-
-        
-        # torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
         optimizer.step()
-        # scheduler.step()
         model.zero_grad()
         # index add
         index += 1
@@ -82,69 +71,47 @@ def train(cfg, epoch, model, loader, optimizer, steps_one_epoch):
             cur_mean_loss = mean_loss / cfg.show_batch
             enum_dataloader.set_description("EP-{} train, batch {} loss is {}".format(epoch, index, cur_mean_loss))
             mean_loss = 0
-            torch.save(model.state_dict(), 'para/fnn_tmp')
+    
+    torch.save(model.state_dict(), os.path.join(cfg.save_path, "model.ep{}".format(epoch)))
 
 def validate(cfg, model, valid_data_loader):
     model.eval()  
         
+    labels = []
+    preds = []
     with torch.no_grad():
-        preds, truths, rks = list(), list(), list()
-        for data in valid_data_loader:
-            # 1. Forward
-            pred = model(data[:, :-2])
-            pred = pred / (torch.sum(pred, dim=1, keepdim=True))
-            pred = pred * torch.arange(pred.size(1)).unsqueeze(0)
-            pred = torch.sum(pred, dim=1)
-            if pred.dim() > 1:
-                pred = pred.squeeze()
-            try:
-                preds += pred.numpy().tolist()
-            except:
-                preds.append(int(pred.cpu().numpy()))
-            truths += data[:, -2].numpy().flatten().tolist()
-            rks += data[:, -1].numpy().flatten().tolist()
-
-        rpreds = np.round(preds)
-        residual = (truths - rpreds).astype("int")
-        loss = np.where(residual < 0, residual * -0.6, residual * 0.4)
-
-        return np.mean(loss), rks, preds
+        for data in tqdm(valid_data_loader, total=len(valid_data_loader), desc="valid"):
+            input_data = data[:, :-1] / 256.0
+            label_data = data[:, -1]
+            input_data = input_data.to(0)
+            res = model(input_data, 224, valid_mode=True)
+            labels += label_data.cpu().numpy().tolist()
+            preds += res.cpu().numpy().tolist()
+    
+    score = roc_auc_score(labels, preds)
+    print(score)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--folds", default=10, type=int)
-parser.add_argument("--epoch", default=1, type=int)
-parser.add_argument("--batch_size", default=256, type=int)
+parser.add_argument("--dpath", default="/mnt/e/data/", type=str,
+                        help="Path of the output dir.")
+parser.add_argument("--epoch", default=6, type=int)
+parser.add_argument("--batch_size", default=32, type=int)
 parser.add_argument("--lr", default=0.001, type=int)
 parser.add_argument("--save_path", default='para', type=str)
 parser.add_argument("--show_batch", default=1000, type=int)
 args = parser.parse_args()
-cate_info = json.load(open('data/category_info.json'))
-args.cate_info = cate_info
 
-loss_and_output = []
-total_rc = []
-total_preds = []
-for i in range(1, args.folds + 1):
-    print('model:', i)
-    train_dataset = FNNData('data/subtrain_cat/train_{}.tsv'.format(i))
-    valid_dataset = FNNData('data/subtrain_cat/valid_{}.tsv'.format(i))
-    cur_loss, rc, preds = run(args, train_dataset, valid_dataset, os.path.join(args.save_path, 'pfnn_{}'.format(i)))
-    loss_and_output.append(cur_loss)
-    total_rc += rc
-    total_preds += preds
-    del train_dataset, valid_dataset
-    gc.collect()
+matrixp = os.path.join(args.dpath, "imageset.npy")
+trainsetp = os.path.join(args.dpath, "train.npy")
+validsetp = os.path.join(args.dpath, "valid.npy")
 
-to_save = []
-for rnumber, predict_value in zip(total_rc, total_preds):
-    to_save.append([rnumber, predict_value])
-savedf = pd.DataFrame(to_save, columns=['record_number', 'pFNN_predict'])
-savedf.to_csv('data/sl_data/pfnn_train.tsv', sep='\t', index=None) 
+args.model_info = GeMConfig()
+pmatrix = torch.ByteTensor(np.load(matrixp))
+trainset = np.load(trainsetp)
+validset = np.load(validsetp)
 
-lao = np.array([1 / x for x in loss_and_output])
-lao = lao / lao.sum()
+train_dataset = GeMData(pmatrix, trainset)
+valid_dataset = GeMData(pmatrix, validset)
+run(args, train_dataset, valid_dataset)
 
-json.dump(list(lao), open('para/pfnn_weight.json', 'w'))
-json.dump(loss_and_output, open('para/all_pfnn_log.json', 'w'))
-print('mean loss:', np.mean(loss_and_output))
 
