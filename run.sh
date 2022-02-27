1f36f0e1ac2f7c5734f05acff36ddb5c49e89db8
xiaoxin Hu <1033718596@qq.com>
2022-02-27 11:58:45 -0800

    lopq

--- run.sh
+++ run.sh
@@ -11,7 +11,10 @@ python -m process_data.test_set_generation --dpath=ir
 
 python train.py --dpath=ir --save_path=ir/para/ --batch_size=64 --epoch=2 --show_batch=5 --lr=0.0001
 python build_image_vector.py --dpath=ir --save_path=ir/para/model.0223.975 --batch_size=256
+
+# Maximum Inner Product Search
 python predict.py --dpath=ir --save_path=ir/para/model.0223.975 --batch_size=256
+# Or LOPQ
 
 
 
