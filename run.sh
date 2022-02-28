5861d4a4708b434adb47b4bc89c633820ea39a4d
xiaoxin Hu <1033718596@qq.com>
2022-02-28 12:01:05 -0800

    typo

--- run.sh
+++ run.sh
@@ -14,7 +14,7 @@ python build_image_db.py --dpath=ir --save_path=ir/para/model.0223.975 --batch_s
 python build_image_db.py --dpath=ir --save_path=ir/para/model.0223.975 --batch_size=256 --input=test_image.npy --output=tdatabase.npy
 # Maximum Inner Product Search
 python mips.py --dpath=ir --batch_size=1024 --k=20
-python mips.py --dpath=ir --batch_size=1024 --k=20 --to_test=valid_for_test.npy --text_matrix=database.npy
+python mips.py --dpath=ir --batch_size=1024 --k=20 --to_test=valid_for_test.npy --test_matrix=database.npy
 # Or LOPQ
 python -m process_data.nns --dpath=ir --dimension=32 --k=20
 
