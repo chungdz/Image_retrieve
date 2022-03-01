f1ce10ac71eea72cf702faa26ebe284c1aabbd4f
xiaoxin Hu <1033718596@qq.com>
2022-03-01 11:24:21 -0800

    no first one

--- run.sh
+++ run.sh
@@ -14,7 +14,7 @@ python build_image_db.py --dpath=ir --save_path=ir/para/model.ep0 --batch_size=2
 python build_image_db.py --dpath=ir --save_path=ir/para/model.ep0 --batch_size=256 --input=test_image.npy --output=tdatabase.npy
 # Maximum Inner Product Search
 python process_data.mips.py --dpath=ir --batch_size=1024 --k=20
-python process_data.mips.py --dpath=ir --batch_size=1024 --k=20 --to_test=valid_for_test.npy --test_matrix=database.npy
+python process_data.mips.py --dpath=ir --batch_size=1024 --k=20 --isValid=1 --to_test=valid_for_test.npy --test_matrix=database.npy
 # Or LOPQ
 python -m process_data.nns --dpath=ir --dimension=32 --k=20
 python -m process_data.nns --dpath=ir --dimension=32 --k=20 --to_test=valid_for_test.npy --test_matrix=database.npy
