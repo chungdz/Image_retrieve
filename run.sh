3125170b86af927579ecb32e6ffe5f93f8f34556
xiaoxin Hu <1033718596@qq.com>
2022-03-01 10:15:58 -0800

    change path into same

--- run.sh
+++ run.sh
@@ -13,8 +13,8 @@ python train.py --dpath=ir --save_path=ir/para/ --batch_size=64 --epoch=3 --show
 python build_image_db.py --dpath=ir --save_path=ir/para/model.ep0 --batch_size=256 --input=imageset.npy --output=database.npy
 python build_image_db.py --dpath=ir --save_path=ir/para/model.ep0 --batch_size=256 --input=test_image.npy --output=tdatabase.npy
 # Maximum Inner Product Search
-python mips.py --dpath=ir --batch_size=1024 --k=20
-python mips.py --dpath=ir --batch_size=1024 --k=20 --to_test=valid_for_test.npy --test_matrix=database.npy
+python process_data.mips.py --dpath=ir --batch_size=1024 --k=20
+python process_data.mips.py --dpath=ir --batch_size=1024 --k=20 --to_test=valid_for_test.npy --test_matrix=database.npy
 # Or LOPQ
 python -m process_data.nns --dpath=ir --dimension=32 --k=20
 python -m process_data.nns --dpath=ir --dimension=32 --k=20 --to_test=valid_for_test.npy --test_matrix=database.npy
