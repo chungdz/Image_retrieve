39696c9e8ecc4ef1aefbbda2304c4b771552f37a
xiaoxin Hu <1033718596@qq.com>
2022-03-01 16:22:02 -0800

    first change

--- process_data/make_train_valid.py
+++ process_data/make_train_valid.py
@@ -12,7 +12,7 @@ random.seed(7)
 parser = argparse.ArgumentParser()
 parser.add_argument("--dpath", default="/mnt/e/data/", type=str,
                         help="Path of the output dir.")
-parser.add_argument("--ratio", default=7, type=int,
+parser.add_argument("--ratio", default=9, type=int,
                         help="#valid over #train.")
 parser.add_argument("--neg_count", default=4, type=int,
                         help="negative count.")
@@ -42,15 +42,6 @@ print(len(cmdict), cmidx)
 
 train_set = []
 valid_set = []
-test_set = []
-all_valid = set()
-for carm_index, pic_set in tqdm(cdict.items(), total=len(cdict), desc='make valid index set'):
-    pic_list = list(pic_set)
-    cur_len = len(pic_list)
-    valid_num = cur_len // args.ratio
-    valid_list = pic_list[:valid_num]
-    for vidx in valid_list:
-        all_valid.add(vidx)
 
 for carm_index, pic_set in tqdm(cdict.items(), total=len(cdict), desc='make train and valid'):
     pic_list = list(pic_set)
@@ -60,38 +51,18 @@ for carm_index, pic_set in tqdm(cdict.items(), total=len(cdict), desc='make trai
     valid_list = pic_list[:valid_num]
     train_list = pic_list[valid_num:]
     
-    for i in range(train_num - 1):
-        for j in range(i + 1, train_num):
-            new_sample = [train_list[i], train_list[j]]
-            while len(new_sample) < 2 + args.neg_count:
-                neg_idx = random.randint(0, end_index - 1)
-                if neg_idx not in pic_set and neg_idx not in new_sample and neg_idx not in all_valid:
-                    new_sample.append(neg_idx)
-            train_set.append(new_sample)
-    
-    pos_sample_valid = random.sample(train_list, valid_num)
-    for i in range(valid_num):
-        new_sample_pos = [valid_list[i], pos_sample_valid[i], 1]
-        new_sample_neg = [valid_list[i]]
-        while len(new_sample_neg) < 2:
-            neg_idx = random.randint(0, end_index - 1)
-            if neg_idx not in pic_set and neg_idx != valid_list[i]:
-                new_sample_neg.append(neg_idx)
-        new_sample_neg.append(0)
-        valid_set.append(new_sample_pos)
-        valid_set.append(new_sample_neg)
-    
-    for pidx in valid_list:
-        test_set.append([pidx, carm_index])
+    for pic_id in train_list:
+        train_set.append([pic_id, carm_index])
+    for pic_id in valid_list:
+        valid_set.append([pic_id, carm_index])
+
 
 train_set = np.array(train_set)
 valid_set = np.array(valid_set)
-test_set = np.array(test_set)
-print(train_set.shape, valid_set.shape, test_set.shape)
+print(train_set.shape, valid_set.shape)
 
 np.save(train_path, train_set)
 np.save(valid_path, valid_set)
-np.save(test_path, test_set)
 
 saved_index = {int(k): [int(x) for x in v] for k, v in cdict.items()}
 json.dump(saved_index, open(dictionary_path, 'w'))
