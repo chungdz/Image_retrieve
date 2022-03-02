9f15d18367a2a8dd2619172cca321593f20a2922
xiaoxin Hu <1033718596@qq.com>
2022-03-02 15:33:07 -0800

    add argue

--- process_data/mask_data.py
+++ process_data/mask_data.py
@@ -1,3 +1,12 @@
 import numpy as np
+import argparse
+import os
 import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--dpath", default="/mnt/e/data/", type=str,
+                        help="Path of the output dir.")
+args = parser.parse_args()
+
+
 
