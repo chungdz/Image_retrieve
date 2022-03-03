e2eabf48792ec989bee1a7ba334b7dc6a7956133
xiaoxin Hu <1033718596@qq.com>
2022-03-02 22:45:17 -0800

    new mips for better results

--- process_data/mips.py
+++ process_data/mips.py
@@ -17,11 +17,11 @@ parser.add_argument("--test_matrix", default="tdatabase.npy", type=str)
 parser.add_argument("--isValid", default=0, type=int)
 args = parser.parse_args()
 
-dbp = os.path.join(args.dpath, "database.npy")
+dbp = os.path.join(args.dpath, "database_masked.npy")
 testp = os.path.join(args.dpath, args.to_test)
 imagep = os.path.join(args.dpath, args.test_matrix)
 classp = os.path.join(args.dpath, "model_num.json")
-resp = os.path.join(args.dpath, "mAP.json")
+resp = os.path.join(args.dpath, "dbcm.json")
 print('load data')
 md = json.load(open(classp, "r"))
 image_matrix = torch.FloatTensor(np.load(imagep))
@@ -57,10 +57,10 @@ print(final_matrix.shape)
 labeled = np.zeros(final_matrix.shape)
 mAP_list = []
 for i in tqdm(range(final_matrix.shape[0]), desc='map to binary and calculate mAP'):
-    cur_s = set(md[str(test_class[i])])
+    # cur_s = set(md[str(test_class[i])])
     plist = []
     for j in range(final_matrix.shape[1]):
-        labeled[i, j] = int(final_matrix[i][j] in cur_s)
+        labeled[i, j] = int(md[final_matrix[i][j]] == int(test_class[i]))
         if labeled[i, j] == 1:
             plist.append(labeled[i, :j + 1].sum() / (j + 1))
 
