0462bee1c68be9244b0757a186672e74544f87c2
xiaoxin Hu <1033718596@qq.com>
2022-03-04 10:47:18 -0800

    add arch info

--- run.sh
+++ run.sh
@@ -9,9 +9,9 @@ python -m process_data.image_matrix --dpath=ir
 python -m process_data.make_train_valid --dpath=ir
 python -m process_data.test_set_generation --dpath=ir
 
-python train.py --dpath=ir --save_path=ir/para/ --batch_size=64 --epoch=3 --show_batch=5 --lr=0.0001 --lr_shrink=0.9
-python build_image_db.py --dpath=ir --save_path=ir/para/model.ep0 --batch_size=256 --input=imageset.npy --output=database.npy
-python build_image_db.py --dpath=ir --save_path=ir/para/model.ep0 --batch_size=256 --input=test_image.npy --output=tdatabase.npy
+python train.py --dpath=ir --save_path=ir/para/ --batch_size=64 --epoch=3 --show_batch=5 --lr=0.0001 --lr_shrink=0.9 --arch=resnet50
+python build_image_db.py --dpath=ir --save_path=ir/para/model.ep0 --batch_size=64 --input=imageset.npy --output=database.npy --arch=resnet50
+python build_image_db.py --dpath=ir --save_path=ir/para/model.ep0 --batch_size=64 --input=test_image.npy --output=tdatabase.npy --arch=resnet50
 python -m process_data.mask_data --dpath=ir
 # Maximum Inner Product Search
 python -m process_data.mips --dpath=ir --batch_size=1024 --k=20 --isValid=1 --to_test=valid_for_test.npy --test_matrix=database.npy
