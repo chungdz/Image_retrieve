ff21588bf0c6f0ff30361e601717afc3a4a8df59
xiaoxin Hu <1033718596@qq.com>
2022-05-11 09:44:10 -0700

    revisit

---
+++ revisitop/run.sh
@@ -0,0 +1,10 @@
+# download revisit oxford dictionary in http://cmp.felk.cvut.cz/revisitop/data/datasets/roxford5k/gnd_roxford5k.pkl
+# download revisit paris dictionary in http://cmp.felk.cvut.cz/revisitop/data/datasets/rparis6k/gnd_rparis6k.pkl
+
+mkdir landmark
+cd landmark
+mkdir oxford5k paris6k
+# download oxford dataset in https://www.robots.ox.ac.uk/~vgg/data/oxbuildings/oxbuild_images-v1.tgz
+# download paris dataset in https://www.robots.ox.ac.uk/~vgg/data/parisbuildings/paris_1-v1.tgz and https://www.robots.ox.ac.uk/~vgg/data/parisbuildings/paris_2-v1.tgz
+# decompress the two datasets and mv all images into two folds separately
+
