3de232570742fe2569d5b4ed08a24620c1c8da75
xiaoxin Hu <1033718596@qq.com>
2022-05-15 00:05:57 -0700

    layer norm for better results

--- run_landmark.sh
+++ run_landmark.sh
@@ -1,29 +1,34 @@
 python -m revisitop.make_test_files --dname=oxford5k --dpath=landmark
 python -m revisitop.make_test_files --dname=paris6k --dpath=landmark
 python -m revisitop.make_train_info
+# whole
 python -m revisitop.merge
-python -m process_data.make_train_valid --dpath=landmark --ratio=16 --iname=train_info.csv --skip=10
+python -m process_data.make_train_valid --dpath=landmark --ratio=16 --iname=train_info_final.csv --skip=10
+# only revisit
+python -m revisitop.merge_no_sfm
+python -m process_data.make_train_valid --dpath=landmark --ratio=16 --iname=revisit_info.csv --skip=10
 
 # using --isM=0 for single gem
 # Swin GeM 224 
-python train_class.py --isM=0 --start_epoch=-1 --dpath=landmark --train_num=5 --img_size=224 --save_path=landmark/para/ --batch_size=64 --epoch=5 --show_batch=5 --lr=0.0001 --lr_shrink=0.9 --arch=swin
+python train_class.py --isM=0 --start_epoch=4 --dpath=landmark --train_num=6 --img_size=224 --save_path=landmark/para/ --batch_size=64 --epoch=8 --show_batch=5 --lr=0.0001 --lr_shrink=0.9 --arch=swin
 # oxford
-python build_image_db.py --isM=0 --dpath=landmark --img_size=224 --save_path=landmark/para/model.ep1 --batch_size=256 --input=oxford5k_dbm.npy --output=ox_database.npy --arch=swin --encoder=gem
-python build_image_db.py --isM=0 --dpath=landmark --img_size=224 --save_path=landmark/para/model.ep1 --batch_size=256 --input=oxford5k_qm.npy --output=ox_tdatabase.npy --arch=swin --encoder=gem
+python build_image_db.py --isM=0 --dpath=landmark --img_size=224 --save_path=landmark/para/model.ep6 --batch_size=256 --input=oxford5k_dbm.npy --output=ox_database.npy --arch=swin --encoder=gem
+python build_image_db.py --isM=0 --dpath=landmark --img_size=224 --save_path=landmark/para/model.ep6 --batch_size=256 --input=oxford5k_qm.npy --output=ox_tdatabase.npy --arch=swin --encoder=gem
 python -m revisitop.mips --db_matrix=ox_database.npy --test_matrix=ox_tdatabase.npy --info_dict=oxford5k_info.json
 # paris
-python build_image_db.py --isM=0 --dpath=landmark --img_size=224 --save_path=landmark/para/model.ep1 --batch_size=256 --input=paris6k_dbm.npy --output=pa_database.npy --arch=swin --encoder=gem
-python build_image_db.py --isM=0 --dpath=landmark --img_size=224 --save_path=landmark/para/model.ep1 --batch_size=256 --input=paris6k_qm.npy --output=pa_tdatabase.npy --arch=swin --encoder=gem
+python build_image_db.py --isM=0 --dpath=landmark --img_size=224 --save_path=landmark/para/model.ep6 --batch_size=256 --input=paris6k_dbm.npy --output=pa_database.npy --arch=swin --encoder=gem
+python build_image_db.py --isM=0 --dpath=landmark --img_size=224 --save_path=landmark/para/model.ep6 --batch_size=256 --input=paris6k_qm.npy --output=pa_tdatabase.npy --arch=swin --encoder=gem
 python -m revisitop.mips --db_matrix=pa_database.npy --test_matrix=pa_tdatabase.npy --info_dict=paris6k_info.json
 # Swin Multi GeM 224 
-python train_class.py --start_epoch=-1 --dpath=landmark --train_num=5 --img_size=224 --save_path=landmark/para/ --batch_size=64 --epoch=3 --show_batch=5 --lr=0.0001 --lr_shrink=0.9 --arch=swin
+python train_class.py --start_epoch=-1 --dpath=landmark --train_num=6 --img_size=224 --save_path=landmark/para/ --batch_size=64 --epoch=10 --show_batch=5 --lr=0.0001 --lr_shrink=0.9 --arch=swin
+# python train_class.py --start_epoch=-1 --dpath=landmark --img_size=224 --save_path=landmark/para/ --batch_size=64 --epoch=10 --show_batch=5 --lr=0.0001 --lr_shrink=0.9 --arch=swin --mfile=revisit_trainset.npy --train_num=1
 # oxford
-python build_image_db.py --dpath=landmark --img_size=224 --save_path=landmark/para/model.ep0 --batch_size=256 --input=oxford5k_dbm.npy --output=ox_database.npy --arch=swin --encoder=gem
-python build_image_db.py --dpath=landmark --img_size=224 --save_path=landmark/para/model.ep0 --batch_size=256 --input=oxford5k_qm.npy --output=ox_tdatabase.npy --arch=swin --encoder=gem
+python build_image_db.py --dpath=landmark --img_size=224 --save_path=landmark/para/model.ep8 --batch_size=256 --input=oxford5k_dbm.npy --output=ox_database.npy --arch=swin --encoder=gem
+python build_image_db.py --dpath=landmark --img_size=224 --save_path=landmark/para/model.ep8 --batch_size=256 --input=oxford5k_qm.npy --output=ox_tdatabase.npy --arch=swin --encoder=gem
 python -m revisitop.mips --db_matrix=ox_database.npy --test_matrix=ox_tdatabase.npy --info_dict=oxford5k_info.json
 # paris
-python build_image_db.py --dpath=landmark --img_size=224 --save_path=landmark/para/model.ep0 --batch_size=256 --input=paris6k_dbm.npy --output=pa_database.npy --arch=swin --encoder=gem
-python build_image_db.py --dpath=landmark --img_size=224 --save_path=landmark/para/model.ep0 --batch_size=256 --input=paris6k_qm.npy --output=pa_tdatabase.npy --arch=swin --encoder=gem
+python build_image_db.py --dpath=landmark --img_size=224 --save_path=landmark/para/model.ep8 --batch_size=256 --input=paris6k_dbm.npy --output=pa_database.npy --arch=swin --encoder=gem
+python build_image_db.py --dpath=landmark --img_size=224 --save_path=landmark/para/model.ep8 --batch_size=256 --input=paris6k_qm.npy --output=pa_tdatabase.npy --arch=swin --encoder=gem
 python -m revisitop.mips --db_matrix=pa_database.npy --test_matrix=pa_tdatabase.npy --info_dict=paris6k_info.json
 
 # Resnet 50 + GEM 224 x 224
@@ -37,11 +42,24 @@ python build_image_db.py --isM=0 --dpath=landmark --img_size=224 --save_path=lan
 python build_image_db.py --isM=0 --dpath=landmark --img_size=224 --save_path=landmark/para/resnet50.singlegem --batch_size=256 --input=paris6k_qm.npy --output=pa_tdatabase.npy --arch=resnet50
 python -m revisitop.mips --db_matrix=pa_database.npy --test_matrix=pa_tdatabase.npy --info_dict=paris6k_info.json
 # Resnet 101 + GEM 224 x 224
+# single gem
+python train_class.py --isM=0 --start_epoch=-1 --dpath=landmark --train_num=6 --img_size=224 --save_path=landmark/para/ --batch_size=64 --epoch=5 --show_batch=5 --lr=0.0001 --lr_shrink=0.9 --arch=resnet101
 # oxford
-python build_image_db.py --dpath=landmark --img_size=224 --save_path=cifar100/para/resnet101.multigem --batch_size=256 --input=oxford5k_dbm.npy --output=ox_database.npy --arch=resnet101
-python build_image_db.py --dpath=landmark --img_size=224 --save_path=cifar100/para/resnet101.multigem --batch_size=256 --input=oxford5k_qm.npy --output=ox_tdatabase.npy --arch=resnet101
+python build_image_db.py --isM=0 --dpath=landmark --img_size=224 --save_path=landmark/para/model.ep5 --batch_size=256 --input=oxford5k_dbm.npy --output=ox_database.npy --arch=resnet101
+python build_image_db.py --isM=0 --dpath=landmark --img_size=224 --save_path=landmark/para/model.ep5 --batch_size=256 --input=oxford5k_qm.npy --output=ox_tdatabase.npy --arch=resnet101
 python -m revisitop.mips --db_matrix=ox_database.npy --test_matrix=ox_tdatabase.npy --info_dict=oxford5k_info.json
 # paris
-python build_image_db.py --dpath=landmark --img_size=224 --save_path=cifar100/para/resnet101.multigem --batch_size=256 --input=paris6k_dbm.npy --output=pa_database.npy --arch=resnet101
-python build_image_db.py --dpath=landmark --img_size=224 --save_path=cifar100/para/resnet101.multigem --batch_size=256 --input=paris6k_qm.npy --output=pa_tdatabase.npy --arch=resnet101
+python build_image_db.py --isM=0 --dpath=landmark --img_size=224 --save_path=landmark/para/model.ep5 --batch_size=256 --input=paris6k_dbm.npy --output=pa_database.npy --arch=resnet101
+python build_image_db.py --isM=0 --dpath=landmark --img_size=224 --save_path=landmark/para/model.ep5 --batch_size=256 --input=paris6k_qm.npy --output=pa_tdatabase.npy --arch=resnet101
+python -m revisitop.mips --db_matrix=pa_database.npy --test_matrix=pa_tdatabase.npy --info_dict=paris6k_info.json
+
+# multi gem
+python train_class.py --start_epoch=-1 --dpath=landmark --train_num=6 --img_size=224 --save_path=landmark/para/ --batch_size=64 --epoch=6 --show_batch=5 --lr=0.0001 --lr_shrink=0.9 --arch=resnet101
+# oxford
+python build_image_db.py --dpath=landmark --img_size=224 --save_path=landmark/para/model.ep2 --batch_size=256 --input=oxford5k_dbm.npy --output=ox_database.npy --arch=resnet101
+python build_image_db.py --dpath=landmark --img_size=224 --save_path=landmark/para/model.ep2 --batch_size=256 --input=oxford5k_qm.npy --output=ox_tdatabase.npy --arch=resnet101
+python -m revisitop.mips --db_matrix=ox_database.npy --test_matrix=ox_tdatabase.npy --info_dict=oxford5k_info.json
+# paris
+python build_image_db.py --dpath=landmark --img_size=224 --save_path=landmark/para/model.ep2 --batch_size=256 --input=paris6k_dbm.npy --output=pa_database.npy --arch=resnet101
+python build_image_db.py --dpath=landmark --img_size=224 --save_path=landmark/para/model.ep2 --batch_size=256 --input=paris6k_qm.npy --output=pa_tdatabase.npy --arch=resnet101
 python -m revisitop.mips --db_matrix=pa_database.npy --test_matrix=pa_tdatabase.npy --info_dict=paris6k_info.json
