0d533720a2c36b25f8fcd350b7a72988fd7cea52
xiaoxin Hu <1033718596@qq.com>
2022-06-01 12:09:40 -0700

    for visualization

--- datasets/config.py
+++ datasets/config.py
@@ -40,22 +40,22 @@ class GeMConfig():
         elif 'deit' in arch:
             self.hidden_size = 768
         elif 'mvit' in arch:
-            self.model_settings_path = os.path.join(self.dpath, 'modules/mvit/MVIT_B_16_CONV.yaml')
-            self.model_pretrained_path = os.path.join(self.dpath, 'mvit_para/IN1K_MVIT_B_16_CONV.pyth')
+            self.model_settings_path = os.path.join(self.dpath, '../modules/mvit/MVIT_B_16_CONV.yaml')
+            self.model_pretrained_path = os.path.join(self.dpath, '../mvit_para/IN1K_MVIT_B_16_CONV.pyth')
             self.hidden_size = 768
             self.hidden_list = [96, 192, 192, 384, 384, 384, 384, 384, 384, 384, 384, 384, 384, 384, 768, 768, 768]
 
 class SwinConfig:
     def __init__(self, dpath):
         self.dpath = dpath
-        self.cfg = os.path.join(self.dpath, 'swin_para/swin_large_patch4_window7_224_22k.yaml')
-        self.changeto1k = os.path.join(self.dpath, 'swin_para/map22kto1k.txt')
+        self.cfg = os.path.join(self.dpath, '../swin_para/swin_large_patch4_window7_224_22k.yaml')
+        self.changeto1k = os.path.join(self.dpath, '../swin_para/map22kto1k.txt')
         self.opts = None
         self.batch_size = 32
         self.data_path = None
         self.zip = True
         self.cache_mode = 'part'
-        self.pretrained = os.path.join(self.dpath, 'swin_para/swin_large_patch4_window7_224_22k.pth')
+        self.pretrained = os.path.join(self.dpath, '../swin_para/swin_large_patch4_window7_224_22k.pth')
         self.resume = None
         self.accumulation_steps = None
         self.use_checkpoint = True
